27e0c407347f94fea7b805695409f1e6d6f1849a
akira-erick <kajitani.motta@gmail.com>
2025-07-01 19:39:01 -0300

    documentation(): add initial documentation to the project

--- rust_utils/src/lib.rs
+++ rust_utils/src/lib.rs
@@ -1,1 +1,8 @@
+//! #Utils library
+//!
+//! This crate provides utility functions for various ocasions.
+//!
+//! ## Modules
+//!
+//! - `values_and_conversions`: Contains functions for converting between different types of values, such as temperature conversions.
 pub mod values_and_conversions;
