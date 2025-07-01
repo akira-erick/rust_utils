af5b6120f04404081e559385ff55c6bf054d9245
akira-erick <kajitani.motta@gmail.com>
2025-07-01 19:32:52 -0300

    feat(values_and_convertions): add temperature converter for celsius, fahrenheit and kelvin

---
+++ rust_utils/src/values_and_conversions/temperature.rs
@@ -0,0 +1,64 @@
+pub fn from_celsius_to_fahrenheit(celsius: f64) -> f64 {
+    celsius * 9.0 / 5.0 + 32.0
+}
+
+pub fn from_fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
+    (fahrenheit - 32.0) * 5.0 / 9.0
+}
+
+pub fn from_celsius_to_kelvin(celsius: f64) -> f64 {
+    celsius + 273.15
+}
+
+pub fn from_kelvin_to_celsius(kelvin: f64) -> f64 {
+    kelvin - 273.15
+}
+
+pub fn from_fahrenheit_to_kelvin(fahrenheit: f64) -> f64 {
+    from_celsius_to_kelvin(from_fahrenheit_to_celsius(fahrenheit))
+}
+
+pub fn from_kelvin_to_fahrenheit(kelvin: f64) -> f64 {
+    from_celsius_to_fahrenheit(from_kelvin_to_celsius(kelvin))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_celsius_to_fahrenheit() {
+        assert_eq!(from_celsius_to_fahrenheit(0.0), 32.0);
+        assert_eq!(from_celsius_to_fahrenheit(100.0), 212.0);
+    }
+
+    #[test]
+    fn test_fahrenheit_to_celsius() {
+        assert_eq!(from_fahrenheit_to_celsius(32.0), 0.0);
+        assert_eq!(from_fahrenheit_to_celsius(212.0), 100.0);
+    }
+
+    #[test]
+    fn test_celsius_to_kelvin() {
+        assert_eq!(from_celsius_to_kelvin(0.0), 273.15);
+        assert_eq!(from_celsius_to_kelvin(100.0), 373.15);
+    }
+
+    #[test]
+    fn test_kelvin_to_celsius() {
+        assert_eq!(from_kelvin_to_celsius(273.15), 0.0);
+        assert_eq!(from_kelvin_to_celsius(373.15), 100.0);
+    }
+
+    #[test]
+    fn test_fahrenheit_to_kelvin() {
+        assert_eq!(from_fahrenheit_to_kelvin(32.0), 273.15);
+        assert_eq!(from_fahrenheit_to_kelvin(212.0), 373.15);
+    }
+
+    #[test]
+    fn test_kelvin_to_fahrenheit() {
+        assert_eq!(from_kelvin_to_fahrenheit(273.15), 32.0);
+        assert_eq!(from_kelvin_to_fahrenheit(373.15), 212.0);
+    }
+}
