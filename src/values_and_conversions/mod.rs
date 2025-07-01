46ab9570ab5de7a616384f8e390fe4943ddba5ca
akira-erick <kajitani.motta@gmail.com>
2025-07-01 20:02:18 -0300

    documentation(temperature_converter): add documentation for each function

--- src/values_and_conversions/mod.rs
+++ src/values_and_conversions/mod.rs
@@ -1,1 +1,6 @@
-pub mod temperature;
+//! This module contains various types and functions for handling values and conversions.
+//! ## Modules
+//! - `temperature_converter`: Contains functions for converting between different temperature scales, such as Celsius, Fahrenheit, and Kelvin.
+
+/// This module provides functions for converting between different temperature scales.
+pub mod temperature_converter;
--- src/values_and_conversions/temperature_converter.rs
+++ src/values_and_conversions/temperature_converter.rs
@@ -1,23 +1,65 @@
+/// Converts celsius to fahrenheit
+/// 
+/// # Examples
+/// ```
+/// let fahrenheit = from_celsius_to_fahrenheit(0.0);
+/// assert_eq!(fahrenheit, 32.0);
+/// ```
 pub fn from_celsius_to_fahrenheit(celsius: f64) -> f64 {
     celsius * 9.0 / 5.0 + 32.0
 }
 
+/// Converts fahrenheit to celsius
+/// 
+/// # Examples
+/// ```
+/// let celsius = from_fahrenheit_to_celsius(32.0);
+/// assert_eq!(celsius, 0.0);
+/// ```
 pub fn from_fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
     (fahrenheit - 32.0) * 5.0 / 9.0
 }
 
+/// Converts celsius to kelvin
+/// 
+/// # Examples
+/// ```
+/// let kelvin = from_celsius_to_kelvin(0.0);
+/// assert_eq!(kelvin, 273.15);
+/// ```
 pub fn from_celsius_to_kelvin(celsius: f64) -> f64 {
     celsius + 273.15
 }
 
+/// Converts kelvin to celsius
+/// 
+/// # Examples
+/// ```
+/// let celsius = from_kelvin_to_celsius(273.15);
+/// assert_eq!(celsius, 0.0);
+/// ```
 pub fn from_kelvin_to_celsius(kelvin: f64) -> f64 {
     kelvin - 273.15
 }
 
+/// Converts fahrenheit to kelvin
+///     
+/// # Examples
+/// ```
+/// let kelvin = from_fahrenheit_to_kelvin(32.0);
+/// assert_eq!(kelvin, 273.15);
+/// ```
 pub fn from_fahrenheit_to_kelvin(fahrenheit: f64) -> f64 {
     from_celsius_to_kelvin(from_fahrenheit_to_celsius(fahrenheit))
 }
 
+/// Converts kelvin to fahrenheit
+/// 
+/// # Examples
+/// ```
+/// let fahrenheit = from_kelvin_to_fahrenheit(273.15);
+/// assert_eq!(fahrenheit, 32.0);
+/// ```
 pub fn from_kelvin_to_fahrenheit(kelvin: f64) -> f64 {
     from_celsius_to_fahrenheit(from_kelvin_to_celsius(kelvin))
 }
