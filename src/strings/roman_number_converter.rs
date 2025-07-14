66620ed5686be7d7a7ee4384c8b723a3bb104383
akira-erick <kajitani.motta@gmail.com>
2025-07-14 20:44:40 -0300

    test(roman_number_converter): add tests to roman number converter

--- src/strings/roman_number_converter.rs
+++ src/strings/roman_number_converter.rs
@@ -1,6 +1,17 @@
-pub fn roman_number_to_int(input: &str) -> i32 {
-    let mut ret = 0;
-
-    ret
+pub fn roman_number_to_int(input: &str) -> Result<i32, &str> {
+    
 }
 
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn should_convert_string_to_int(){
+        assert_eq!(roman_number_to_int("X").unwrap(), 10);
+        assert_eq!(roman_number_to_int("XII").unwrap(), 12);
+        assert_eq!(roman_number_to_int("IIX").unwrap(), 8);
+        assert_eq!(roman_number_to_int("MDCLXVI").unwrap(), 1666);
+        assert_eq!(roman_number_to_int("MMMCMXCIX").unwrap(), 3999);
+    }
+}
